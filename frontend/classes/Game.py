b3c721a7e683eaa4b7aeccdb13f04a3daf660f0f
Parrot User <user@localhost.localdomain>
2024-06-17 02:36:23 +0200

    add: adicionei a classe Game e retirei as responsabilidades do frontend.py

---
+++ frontend/classes/Game.py
@@ -0,0 +1,46 @@
+import pygame
+from classes.Player import Player
+from classes.Bullet import Bullet
+from classes.Asteroid import Asteroid
+from classes.Screen import Screen
+
+class Game():
+    def __init__(self):
+        pygame.init()
+        self._window_height = 1280
+        self._window_width = 720
+        self._screen = Screen(self._window_height, self._window_width)
+        self._player = Player(self._window_width / 2, self._window_height / 2)
+        self._clock = pygame.time.Clock()
+        self._running = True
+        self._dt = 0
+
+
+    def run(self):
+        while self._running:
+            # poll for events
+            # pygame.QUIT event means the user clicked X to close your window
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    self._running = False
+                if not self._player.death and event.type == pygame.KEYUP:
+                    self._player.stoped()
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_ESCAPE:
+                        self._running = False
+                    if not self._player.death and event.key == pygame.K_SPACE:
+                        self._player.shoot(self._dt)
+
+            self._screen.render([*Bullet.bullets, *Asteroid.asteroids, self._player])
+            if not self._screen.is_player_colided and not self._screen.update_collisions(self._player, Asteroid.asteroids, Bullet.bullets):
+                self._player.movement(self._dt, pygame.key.get_pressed())
+                Bullet.update_bullets(self._dt)
+                Asteroid.update_asteroid(self._dt, self._screen.width, self._screen.height)
+            else:
+                self._player.explode(self._dt)
+
+            # limits FPS to 60
+            # dt is delta time in seconds since last frame, used for framerate-
+            # independent physics.
+            self._dt = self._clock.tick(60) / 1000
+        pygame.quit()
--- frontend/frontend.py
+++ frontend/frontend.py
@@ -1,57 +1,5 @@
-from math import sin, cos, radians
-from time import sleep
-import pygame
-from classes.Player import Player
-from classes.Bullet import Bullet
-from classes.Asteroid import Asteroid
-from classes.Screen import Screen
-
+from classes.Game import Game 
 
 
 if __name__ == "__main__":
-
-    # pygame setup
-    pygame.init()
-    screen = Screen(1280, 720)
-    clock = pygame.time.Clock()
-    running = True
-    dt = 0
-
-    player = Player(screen.width / 2, screen.height / 2)
-
-    asteroide_group = pygame.sprite.Group()
-    bullet_group = pygame.sprite.Group()
-    asteroide_group = Asteroid.asteroids
-    bullet_group = Bullet.bullets
-
-    while running:
-        # poll for events
-        # pygame.QUIT event means the user clicked X to close your window
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                running = False
-            if not player.death and event.type == pygame.KEYUP:
-                player.stoped()
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_ESCAPE:
-                    running = False
-                if not player.death and event.key == pygame.K_SPACE:
-                    player.shoot(dt)
-
-        # fill the screen with a color to wipe away anything from last frame
-        keys = pygame.key.get_pressed()
-
-        screen.render([*Bullet.bullets, *Asteroid.asteroids, player])
-        if not screen.is_player_colided and not screen.update_collisions(player, asteroide_group, bullet_group):
-            player.movement(dt, keys)
-            Bullet.update_bullets(dt)
-            Asteroid.update_asteroid(dt, screen.width, screen.height)
-        else:
-            player.explode(dt)
-
-        # limits FPS to 60
-        # dt is delta time in seconds since last frame, used for framerate-
-        # independent physics.
-        dt = clock.tick(60) / 1000
-
-    pygame.quit()
+    Game().run()
