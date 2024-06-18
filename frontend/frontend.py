d5f2e97650a4346100acd20446c28be52fa7d7f3
Luiz Eduardo Adami <luizedua@student.42sp.org.br>
2024-06-18 11:56:29 -0300

    final-menu

--- frontend/frontend.py
+++ frontend/frontend.py
@@ -1,14 +1,4 @@
-from classes.Game import Game 
-import pygame_menu
+from classes.Menu import Menu
 
 if __name__ == "__main__":
-    def run_game(game):
-        game.run().quit()
-
-
-    game = Game()
-    menu = pygame_menu.Menu('Asteroids', 400, 300, theme=pygame_menu.themes.THEME_DARK)
-    menu.add.text_input('Name :', default='John Doe')
-    menu.add.button('Play', run_game, game)
-    menu.add.button('Quit', pygame_menu.events.EXIT)
-    menu.mainloop(game._surface)
+    Menu().run()
